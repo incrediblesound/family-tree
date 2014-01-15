7ee9f4ac5a8c3f5ad5e60a0ba2ab4971e33632a4
James H. Edwards <james.howe.edwards@gmail.com>
2014-01-15 22:29:22 +0800

    initial commit

---
+++ family-data.js
@@ -0,0 +1,87 @@
+var familyData = [
+  {   
+    gen: 'a1',
+    name: 'Baron Lucas Gottneb of Voltzgrad',
+    life: '1412-1457',
+    history: 'Baron Gottneb ruled the kingdom of Voltzgrad from birth till death. He is the first\
+    Gottneb of which there are any records. He lived in a manor situated on the edge of a large hunting\
+    park that was surrounded on three sides by mountains. He was a kind ruler, according to all\
+    contemporary references, of which there are but few. He was recorded to have provided grain\
+    to the people during the great famine of 1431, and held an open banquet for the kindgom on the day\
+    of his wedding. He married Cecilia of Weinsolft in the spring of 1433, and subsequently minted coins\
+    in the name of "Lucas and Cecilia, Baron and Baroness of Voltzgrad". They had seven children during their\
+    twenty four years of marriage, but only three survived childhood. The Baron was fond of hunting\
+    and of breeding dogs. Some say the decline of the house of Gottned began in the summer of 1448 when\
+    a pack of dogs mysteriously escaped their kennel and devoured his seventh child while he was playing\
+    in the gardens. The Baron subsequently had all the dogs killed. The killing of the dogs was done hastily\
+    and was overheard by Cecilia. Cecilia was unable to drive the sound of the massacre from her mind and it slowly drove her mad.\
+    She threw herself out of a tower window by that winter.'
+  },
+  {
+    gen: 'a2',
+    name: 'Cecilia Gottneb of Voltzgrad',
+    life: '1422-1448',
+    history: 'The early history of Cecilia Gottneb is unknown. She most likely came from the royal family\
+    of Weinsolft, for it is in this way that political alliances were formed in that time period. She was\
+    well educated in Latin and Greek, and there are a few volumes of hers that are still preserved in the Voltzgrad\
+    museum. The death of four of her children during her marriage with Lucas took a heavy toll on her heart.\
+    In the later years of her life, and she was known to stay up very late reading. In fact, they were tales of\
+    ghost in the manor that would roam the halls at midnight in a white dress. In reality it was probably just\
+    cecilia, who wandered the manor aimlessly in her sleepless depression. Her gloom finally came to an end in\
+    1448 when she killed herself after the death of her sevent child.'
+  },
+  {
+    gen: 'b1', 
+    name:'Baron Edgar Gottneb of Voltzgrad',
+    life: '1434-1478',
+    history:'Edgar Gottned was a prodigal child. Under the tutelage of his father and of the great logician\
+    Abelgrad, his private tutor, Edgar made great accomplishments in poetry, mathematics and astronomy by\
+    the time he was thirteen. Scandal struck early, however, when it was discovered that he had been sneaking\
+    into a nearby village and having an affair with a farm girl when he was just fifteen. He was severely\
+    disciplined by his father, after which he did not leave Gottned manor for at least three years. After his\
+    fathers death Franz became obsessed with music. He had an Italian harpsicord brought to the manor and\
+    placed in one of the minor halls. This period of time was considered the last golden era of the Gottnebs.\
+    Musicians came frequently to the manor, and Edgar and his wife hosted regular salons for poets, composers\
+    and philosophers. It is said that one day Edgar went on a fishing excursion and when he returned did not\
+    speak for ten days. After this time he became increasingly reclusive, and some say he had a secret laboratory\
+    in the basement of the manor where he conducted alchemical experiments. In (????) he comisssioned the\
+    construction of an organ in the chapel of the Manor with a row of pipes that would produce the sound of\
+    a human choir. The experiment went terribly wrong, however, and the pipes produced a raspy sound\
+    somewhat akin to human screams, but with a slightly more animal character. Edgar had the engineer killed.\
+    It is said he spent the rest of his life attempting to compose an organ piece which he never finished,\
+    but evidence of such a work was never found.'
+  },
+  {
+    gen: 'b2', 
+    name: 'Catherine Gottneb',
+    life: '',
+    history: 'Catherine Gottneb, wife of Edgar Gottneb was a woman beloved by all who knew her. She came from\
+    the local nobility and was well educated at a young age. It was said that her only flaw was her husband,\
+    who had a reputation for mental instability. It never upset her, or at least she never showed any discontentment, and always maintained regal poise. After her death if was\
+    revealed that she had an unusual passion for surgery. She had arranged to have private doctor seretly bring\
+    cadavers to Gottned manor for her to study. Other historians have suggested that this fact could explain\
+    some of the more fantastic rumors about her husband\'s experiments.'
+      },
+    {
+      gen: 'c1',
+      name: 'David Gottneb',
+      life:'',
+      history: 'He was a jerk'
+    },
+  {
+    gen: 'c2', 
+    name: 'Baron Edgar Gottneb II of Voltzgrad',
+    life: '',
+    history:'Edgar Gottneb was extremely stern even as a young boy. He was tall and slim, with a narrow face and a piercing gaze that never seemed to change into a smile.\
+    In his early years he spent much of his time\
+    with an old nun who was his tutor, and later on frequently attended the criminal court of the kingdom. It was\
+    said that he had an interest in law, but the events of his life suggest that this was misinformation spread\
+    by the Gottneb family in order to protect their son. In reality, Edgar Gottneb was interested in crime.\
+    He became a military general in (????) and was known for his ruthlessness both toward the enemy and his own\
+    troops. He was liberal in dispensing punishments, to say the least. He had perhaps the worst reputation\
+    of all the Gottnebs despite his rather ordinary life considering who he was and the time he lived. The last\
+    straw fell when he had a peasant drawn and quartered for a minor trade violation. After that he was dragged\
+    from the manor and burned alive by an angry mob.' 
+
+  }
+];
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Family Tree
+
+Family tree is a fictional royal lineage written for entertainment and to practice the fundamentals of Backbone.js 
